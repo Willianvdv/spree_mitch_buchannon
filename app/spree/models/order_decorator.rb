617a8e8b95680240b7ceb77094e80ebb9224ba17
Willian van der Velde <willian@pythonheads.nl>
2013-10-29 16:15:41 +0100

    Filter out orders that are too old and too young

--- app/spree/models/order_decorator.rb
+++ app/spree/models/order_decorator.rb
@@ -1,5 +1,7 @@
 Spree::Order.class_eval do
   def self.payment_reminder_candidates
-    self.complete.where('payment_reminder_sent_at is null')
+    self.complete
+      .where('payment_reminder_sent_at is null')
+      .where(completed_at: 1.days.ago..1.hour.ago)
   end
 end
--- spec/models/spree/order_decorator_spec.rb
+++ spec/models/spree/order_decorator_spec.rb
@@ -7,6 +7,8 @@ describe Spree::Order do
     describe 'with no reminders sent' do
       before :each do
         @completed_order = create :completed_order_with_totals
+        @completed_order.completed_at = 1.hour.ago
+        @completed_order.save!
         @uncompleted_order = create :order
         payment = create :payment
         @paid_order = payment.order
@@ -22,6 +24,32 @@ describe Spree::Order do
 
       it 'should not return unpaid orders' do
         expect(subject).not_to include(@paid_order)
+      end
+
+
+      # Refactor: Think of a nicer way to do these tests. I want
+      # to test if the .payment_reminder_candidates filters out orders
+      # older than 1 day and younger than 1 hour
+      describe 'a order completed a second ago' do
+        before :each do
+          @completed_order.completed_at = 1.second.ago
+          @completed_order.save!
+        end
+
+        it 'should not be a reminder candiate' do
+          expect(subject).to eq([])
+        end
+      end
+
+      describe 'a order completed 7 days ago' do
+        before :each do
+          @completed_order.completed_at = 7.days.ago
+          @completed_order.save!
+        end
+
+        it 'should not be a reminder candiate' do
+          expect(subject).to eq([])
+        end
       end
     end
 
