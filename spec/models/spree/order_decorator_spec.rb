c004499c00718e1adaef68546a3cc4d470c4b780
Willian van der Velde <willian@pythonheads.nl>
2013-10-29 15:48:37 +0100

    Payment reminder candidates should not return paid orders

--- spec/models/spree/order_decorator_spec.rb
+++ spec/models/spree/order_decorator_spec.rb
@@ -5,14 +5,18 @@ describe Spree::Order do
     before :each do
       @completed_order = create :completed_order_with_totals
       @uncompleted_order = create :order
-
+      payment = create :payment
+      @paid_order = payment.order
     end
 
     subject { Spree::Order.payment_reminder_candidates }
 
-    it 'should return completed orders' do
-      expect(subject).to eq([@completed_order])
+    it 'should not return uncompleted orders' do
+      expect(subject).not_to include(@uncompleted_order)
     end
-    
+
+    it 'should not return unpaid orders' do
+      expect(subject).not_to include(@paid_order)
+    end
   end  
 end
