3c602216cef3e6e93e62884853c24a6bca801f0f
Willian van der Velde <willian@pythonheads.nl>
2013-11-11 11:36:04 +0100

    Indent fix

--- app/mailers/spree/payment_reminder_mailer.rb
+++ app/mailers/spree/payment_reminder_mailer.rb
@@ -1,5 +1,5 @@
 class Spree::PaymentReminderMailer < ActionMailer::Base
-    def payment_reminder_email(order)
+  def payment_reminder_email(order)
     @order = order
     
     mail(to: @order.email, subject: t('payment_reminder.payment_reminder_email.subject')) do |format|
