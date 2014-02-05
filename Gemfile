c5fb51918e6403b508685ebb436cc6b1747de8bb
Willian van der Velde <willian@pythonheads.nl>
2014-02-05 13:58:50 +0100

    Spree 2-1-stable preparations

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'http://rubygems.org'
 
+gem 'coveralls', require: false
+
 group :test do
   gem 'email_spec'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,18 @@
+require 'coveralls'
+Coveralls.wear!
+
+# Run Coverage report
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'spec/dummy'
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Mailers', 'app/mailers'
+  add_group 'Models', 'app/models'
+  add_group 'Views', 'app/views'
+  add_group 'Libraries', 'lib'
+end
+
 # Configure Rails Environment
 ENV['RAILS_ENV'] = 'test'
 
@@ -11,10 +26,13 @@ require 'ffaker'
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 
 # Requires factories defined in spree_core
-require 'spree/core/testing_support/factories'
-require 'spree/core/testing_support/controller_requests'
-require 'spree/core/testing_support/authorization_helpers'
-require 'spree/core/url_helpers'
+require 'spree/testing_support/factories'
+require 'spree/testing_support/controller_requests'
+require 'spree/testing_support/authorization_helpers'
+require 'spree/testing_support/url_helpers'
+require 'spree/testing_support/capybara_ext'
+
+#require 'spree_mitch_buchannon/factories'
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
@@ -25,7 +43,7 @@ RSpec.configure do |config|
   #
   # visit spree.admin_path
   # current_path.should eql(spree.products_path)
-  config.include Spree::Core::UrlHelpers
+  config.include Spree::TestingSupport::UrlHelpers
 
   # == Mock Framework
   #
@@ -35,12 +53,22 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
   config.mock_with :rspec
+  config.color = true
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+  # Capybara javascript drivers require transactional fixtures set to false, and we use DatabaseCleaner
+  # to cleanup after each test instead.  Without transactional fixtures set to false the records created
+  # to setup a test will be unavailable to the browser, which runs under a seperate server instance.
+  config.use_transactional_fixtures = false
+
+
+  config.include FactoryGirl::Syntax::Methods
+
+  config.include Spree::TestingSupport::Preferences
+  config.include Spree::TestingSupport::UrlHelpers
+  config.include Spree::TestingSupport::ControllerRequests
+  
+  config.fail_fast = ENV['FAIL_FAST'] || false
 end
