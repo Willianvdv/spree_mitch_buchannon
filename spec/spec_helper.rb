fed2d580c854c329ac92f5350ba804404d14463a
Willian van der Velde <mail@willian.io>
2014-08-07 10:59:06 +0200

    Fixed specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,6 +32,8 @@ require 'spree/testing_support/controller_requests'
 require 'spree/testing_support/authorization_helpers'
 require 'spree/testing_support/url_helpers'
 require 'spree/testing_support/capybara_ext'
+require 'spree/testing_support/preferences'
+
 
 #require 'spree_mitch_buchannon/factories'
 
@@ -70,7 +72,7 @@ RSpec.configure do |config|
   config.include Spree::TestingSupport::Preferences
   config.include Spree::TestingSupport::UrlHelpers
   config.include Spree::TestingSupport::ControllerRequests
-  
+
   config.fail_fast = ENV['FAIL_FAST'] || false
 
   # Ensure Suite is set to use transactions for speed.
