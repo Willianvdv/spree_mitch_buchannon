e15091b9cc9d378cfc0f1e91b21f01f016425246
Rein Aris <info@reinaris.nl>
2014-11-04 17:40:52 +0100

    Extend BaseMailer and added from_address

--- app/mailers/spree/payment_reminder_mailer.rb
+++ app/mailers/spree/payment_reminder_mailer.rb
@@ -1,9 +1,11 @@
-class Spree::PaymentReminderMailer < ActionMailer::Base
-  def payment_reminder_email(order)
-    @order = order
-    
-    mail(to: @order.email, subject: t('payment_reminder.payment_reminder_email.subject')) do |format|
-      format.html
+module Spree
+  class PaymentReminderMailer < BaseMailer
+    def payment_reminder_email(order)
+      @order = order
+      
+      mail(to: @order.email, from: from_address, subject: t('payment_reminder.payment_reminder_email.subject')) do |format|
+        format.html
+      end
     end
   end
-end 
+end
